6a10e5848d163a31172e715bc80580ff79d3e87b
Fabricio Flores <fabrifloresg@gmail.com>
2016-09-30 13:42:32 +0200

    Fix codeclimate

--- klondike/klondike/models/Deck.java
+++ klondike/klondike/models/Deck.java
@@ -18,39 +18,6 @@ public class Deck {
 	    }
 	}
 	
-	public Deck (){
-		this.deck = new ArrayList<Card>();
-	    for (int i=0; i<13; i++){
-	      Number value = Number.values()[i];
-	      for (int j=0; j<4; j++){
-	        Card card = new Card(value, Suit.values()[j]);
-	        this.deck.add(card);
-	      }
-	    }
-	}
-	
-	public Deck (){
-		this.deck = new ArrayList<Card>();
-	    for (int i=0; i<13; i++){
-	      Number value = Number.values()[i];
-	      for (int j=0; j<4; j++){
-	        Card card = new Card(value, Suit.values()[j]);
-	        this.deck.add(card);
-	      }
-	    }
-	}
-	
-	public Deck (){
-		this.deck = new ArrayList<Card>();
-	    for (int i=0; i<13; i++){
-	      Number value = Number.values()[i];
-	      for (int j=0; j<4; j++){
-	        Card card = new Card(value, Suit.values()[j]);
-	        this.deck.add(card);
-	      }
-	    }
-	}
-	
 	public void shuffle(){
 		Collections.shuffle(this.deck);
 	}
