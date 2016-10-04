583ab2d1158758b692d7cbdb9d67ff6a6a92b8b2
Fabricio Flores <fabrifloresg@gmail.com>
2016-10-04 22:17:39 +0200

    Remove magic number

--- src/main/java/klondike/models/StackDeck.java
+++ src/main/java/klondike/models/StackDeck.java
@@ -3,10 +3,12 @@ package klondike.models;
 import java.util.Collections;
 
 public class StackDeck extends StackCard {
+	
+	public static final int NUMBER_LIMIT = 13;
 
 	public StackDeck() {
 		super();
-		for (int i = 0; i < 13; i++) {
+		for (int i = 0; i < NUMBER_LIMIT; i++) {
 			Number value = Number.values()[i];
 			for (int j = 0; j < 4; j++) {
 				Card card = new Card(value, Suit.values()[j], true);
